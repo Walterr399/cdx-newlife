b12b5395afbcf31f8aee4e63a63dbaf2ae7184fa
Walterr399 <walterwhiterp47@gmail.com>
2025-01-01 17:03:10 +0100

    Delete cdx-newlife directory

--- cdx-newlife/configs/config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-Config.RespawnLocations = {
-    { name = "Ziekenhuis", coords = vector3(295.9224, -584.8321, 43.1427), time = 1 },
-    { name = "Begravenis", coords = vector3(-1725.1920, -281.6174, 50.3148), time = 2 },
-    { name = "Paleto", coords = vector3(-283.1532, 6026.6812, 31.4945), time = 5 }
-}
--- cdx-newlife/server/server.lua
+++
@@ -1,8 +0,0 @@
-RegisterNetEvent('cdx-newlife:revivePlayer')
-AddEventHandler('cdx-newlife:revivePlayer', function(coords)
-    local player = source
-    if player then
-        TriggerClientEvent('frp-ambulance:client:staffrevive:player', player) -- Revive trigger maak ticket aan indien je het zelf niet kan aanpassen
-        TriggerClientEvent('cdx-newlife:teleportPlayer', player, coords)
-    end
-end)
